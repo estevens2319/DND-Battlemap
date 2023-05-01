c63ea9ffb8e9e8b86c851867797ad082100ed4c3
Eric Stevens <uspeople@icloud.com>
2023-05-01 17:29:45 -0400

    Changed how to move images

--- index.js
+++ index.js
@@ -1,96 +1,89 @@
 let character = "eric";
-let facingRight = true;
 let speed = 1;
 let charImg = document.getElementById(character);
-let characterList = ["eric", "jack", "matt", "haylee", "nice", "adam", "travis", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10"];
-let follow = false;
-let charClicked = true;
-let prone = false;
-let stand = false;
-let smallerSwitch = false;
-let biggerSwitch = false;
-document.addEventListener("click", mouseClicked);
-document.onmousemove = doaflip;
-  
-function mouseClicked(){
-  if(charClicked){
-    follow = true;
-    charClicked = false;
-  }
-  else{
-  follow = false;
-  }
+let characterList = ["eric", "jack", "matt", "haylee", "nicole", "adam", "travis", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10"];
+let mouseClickX = 0;
+let mouseClickY = 0;
+let mouseClickOffsetX = 0;
+let mouseClickOffsexY = 0; 
+
+function mouseReleased() {
+  window.removeEventListener('mousemove', move, true);
 }
 
-function doaflip(event) {
-  if (follow) {
-    event = event || window.event;
-    let y = event.pageY + 1;
-    let x = event.pageX + 1;
-    charImg.style.top = y + 'px';
-    charImg.style.left = x + 'px';
-  }
+function mouseClicked(e, char) {
+  chooseCharacter(char);
+  mouseClickX = e.clientX;
+  mouseClickY = e.clientY;
+  var div = document.getElementById(character);
+  let leftPart = "";
+  if (!div.style.left)
+    leftPart += "0px";  
+  else
+    leftPart = div.style.left;
+  let leftPos = leftPart.indexOf("px");
+  let leftNumString = leftPart.slice(0, leftPos); 
+  mouseClickOffsetX = mouseClickX - parseInt(leftNumString, 10);
+  let topPart = "";
+  if (!div.style.top)
+    topPart += "0px";   
+  else
+    topPart = div.style.top;
+  let topPos = topPart.indexOf("px");
+  let topNumString = topPart.slice(0, topPos);  
+  mouseClickOffsexY = mouseClickY - parseInt(topNumString, 10);
+  window.addEventListener('mousemove', move, true);
 }
 
-
+function move(e) {
+  var div = document.getElementById(character);
+  div.style.position = 'absolute';
+  let topAmount = e.clientY - mouseClickOffsexY;
+  div.style.top = topAmount + 'px';
+  let leftAmount = e.clientX - mouseClickOffsetX;
+  div.style.left = leftAmount + 'px';
+}
 
 function showRules(){
-  let rules = "How to navigate the map." + '\n' + "To change background:" + '\n' + "Click the background button and select a file for the background." + '\n' + "To move a character:" + '\n' + "Click on a character and the character will follow the mouse until you click again." + '\n' + "To add new characters:" + '\n' + "Click a C# button to add or change the icon of that character number" + '\n' 
-    + "To make a character prone: (Uploaded characters cannot go prone) " + '\n' + "Press P key, click on a character" + '\n' + "To make a character stand: " + '\n' + "Press S key, click on a character" + '\n' + "To enlarge / shrink a character" + '\n' + "Press =/- key. Click a character until they are the desired size. Press =/- key again";
+  let rules = "How to navigate the map." + '\n' + "To change background:" + '\n' + "Click the background button and select a file for the background." + '\n' + "To move a character:" + '\n' + "Click and drag a character around map or use arrow keys." + '\n' + "To add new characters:" + '\n' + "Click a C# button to add or change the icon of that character number" + '\n' 
+    + "To make a character prone: (Uploaded characters cannot go prone) " + '\n' + "Click on a character, press P key" + '\n' + "To make a character stand: " + '\n' + "Click on a character, press S key" + '\n' + "To enlarge / shrink a character" + '\n' + "Click a character, press =/- key.";
   alert(rules);
-
 }
+
 function bigger(charImg){
   charImg.width = JSON.stringify(charImg.width + 10);
 }
+
 function smaller(charImg){
 charImg.width = JSON.stringify(charImg.width - 10);
 }
+
 function chooseCharacter(charName) {
   character = charName;
   charImg = document.getElementById(character);
-  if (prone) {
-    goProne(charImg)
-  }
-  else if (stand){
-    goUnProne(charImg);
-  }
-  else if (biggerSwitch){
-    bigger(charImg)
-  }
-  else if (smallerSwitch){
-    smaller(charImg)
-  }
-  else{
-    charClicked = true;
-  }
 }
 
 function goProne(charImg) {
-    prone = false;
     if(character[0] !== "C")
       charImg.src = "./images/" + character + "prone.png";
 }
-function goUnProne(charImg) {
-    stand = false;
+
+function stand(charImg) {
     if(character[0] !== "C")
       charImg.src = "./images/" + character + ".png";
 }
 
 function changeBG(input) {
-
   document.getElementById('bg').style.display = "block";
-
   if (input.files && input.files[0]) {
     var reader = new FileReader();
-
     reader.onload = function (e) {
       document.getElementById('bg').src = e.target.result;
     }
-
     reader.readAsDataURL(input.files[0]);
   }
 }
+
 function changeC(input, C) {
   document.getElementById(C).style.display = "block";
   if (input.files && input.files[0]) {
@@ -99,26 +92,6 @@ function changeC(input, C) {
       document.getElementById(C).src = e.target.result;
     }
     reader.readAsDataURL(input.files[0]);
-  }
-}
-
-
-function getPosition(charToGet) {
-  charToGet = document.getElementById(charToGet);
-  return [charToGet.offsetLeft, charToGet.offsetTop];
-}
-
-
-function characterSwitch() {
-  currPosition = getPosition(character);
-  for (let i = 0; i < characterList.length; i++) {
-    if (characterList[i] !== character) {
-      let nextChar = getPosition(characterList[i]);
-      if ((Math.abs(currPosition[0] - nextChar[0]) < charImg.clientHeight) && (Math.abs(currPosition[1] - nextChar[1]) < (charImg.clientWidth))) {
-        chooseCharacter(characterList[i]);
-        return;
-      }
-    }
   }
 }
 
@@ -168,20 +141,15 @@ function downArrowPressed(charImg) {
   }
 }
 
-function reset(charImg) {
-  charImg.style.top = '100px';
-  charImg.style.left = '100px';
-}
-
-
-
-function fire(charImg) {
-  characterSwitch();
-}
-
 function docReady() {
-  window.addEventListener('keydown', checkXP);
-
+  for (let i = 0; i < characterList.length; i++) {
+    let image = document.getElementById(characterList[i]);
+    image.addEventListener('mousedown', function (e) {
+      mouseClicked(e, characterList[i]);
+    }, false);
+  }
+  window.addEventListener('mouseup', mouseReleased, false);
+  window.addEventListener('keydown', checkKey);
   function KeyboardController(keys, repeat) {
     var timers = {};
     document.onkeydown = function (event) {
@@ -212,32 +180,27 @@ function docReady() {
     };
   };
   charImg = document.getElementById(character);
-  function checkXP(key) {
-    if (key.keyCode === 88) {
-      fire(charImg);
-    }
+  function checkKey(key) {
     if (key.keyCode === 80) {
-      prone = true;
+      goProne(charImg)
     }
     if (key.keyCode === 83) {
-      stand = true;
+      stand(charImg)
     }
     if (key.keyCode === 77) {
       clickMove(charImg);
     }
     if (key.keyCode === 187) {
-      smallerSwitch = false;
-      biggerSwitch = !biggerSwitch;
+      bigger(charImg);
     }
     if (key.keyCode === 189) {
-      biggerSwitch = false;
-      smallerSwitch = !smallerSwitch;
+      smaller(charImg);
     }
   }
   KeyboardController({
     37: function () { leftArrowPressed(charImg); },
     38: function () { upArrowPressed(charImg); },
     39: function () { rightArrowPressed(charImg); },
-    40: function () { downArrowPressed(charImg); }
+    40: function () { downArrowPressed(charImg); },
   }, 1);
 }
